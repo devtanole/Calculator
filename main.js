17970fd16e45a7d5ed1158a02da8f559d384d4d2
Devin Olesen <devintanielu@gmail.com>
2025-07-25 18:31:25 +0000

    operation functions and state

---
+++ main.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
+
+function round(number) {
+  return Math.round(number * 1000) / 1000;
+}
